a227116799e0923ae1166f5fd162ba6d30e3f921
hqz <2462546443@qq.com>
2022-12-05 22:08:18 +0800

    lab5

--- os/src/sbi.rs
+++ os/src/sbi.rs
@@ -27,6 +27,10 @@ fn sbi_call(which: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
     ret
 }
 
+pub fn set_timer(timer: usize) {
+    sbi_call(SBI_SET_TIMER, timer, 0, 0);
+}
+
 pub fn console_putchar(c: usize) {
     sbi_call(SBI_CONSOLE_PUTCHAR, c, 0, 0);
 }
@@ -38,14 +42,4 @@ pub fn console_getchar() -> usize {
 pub fn shutdown() -> ! {
     sbi_call(SBI_SHUTDOWN, 0, 0, 0);
     panic!("It should shutdown!");
-}
-
-//os/src/sbi.rs
-use crate::sbi::set_timer;
-use crate::config::CLOCK_FREQ;
-
-const TICKS_PER_SEC: usize = 100;
-
-pub fn set_next_trigger() {
-        set_timer(get_time() + CLOCK_FREQ / TICKS_PER_SEC);
 }
