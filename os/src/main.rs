1476d35b6faa255adf44e971d3ee5d45b13bb2ab
hqz <2462546443@qq.com>
2022-12-12 19:36:04 +0800

    last commit of lab6

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,8 +1,14 @@
 #![no_std]
 #![no_main]
 #![feature(panic_info_message)]
+#![feature(alloc_error_handler)]
 
 use core::arch::global_asm;
+
+extern crate alloc;
+
+#[macro_use]
+extern crate bitflags;
 
 #[macro_use]
 mod console;
@@ -14,6 +20,7 @@ mod loader;
 mod config;
 mod task;
 mod timer;
+mod sync;
 mod mm;
 
 global_asm!(include_str!("entry.asm"));
@@ -24,22 +31,24 @@ fn clear_bss() {
         fn sbss();
         fn ebss();
     }
-    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+    unsafe {
+        core::slice::from_raw_parts_mut(
+            sbss as usize as *mut u8,
+            ebss as usize - sbss as usize,
+        ).fill(0);
+    }
 }
 
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
-    println!("[Kernel] Hello, world!");
-    trap::init();
+    println!("[kernel] Hello, world!");
     mm::init();
-    loader::load_apps();
+    println!("[kernel] back to world!");
+    mm::remap_test();
+    trap::init();
     trap::enable_timer_interrupt();
     timer::set_next_trigger();
     task::run_first_task();
     panic!("Unreachable in rust_main!");
 }
-extern crate alloc;
-#![feature(alloc_error_handler)]
-#[macro_use]
-extern crate bitflags;
